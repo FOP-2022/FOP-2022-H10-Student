5915c69fb15ed624a9da79c6f4a2b1253884d757
Alexander Staeding <alexanderstaeding@gmail.com>
2021-12-21 23:33:34 +0100

    Fix formatting

--- src/main/java/h10/Main.java
+++ src/main/java/h10/Main.java
@@ -2,7 +2,7 @@ package h10;
 
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
