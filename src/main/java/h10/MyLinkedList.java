b9bc7d0871733bb34d9c6213a8951cc1bd127874
paltunay <paulaltunay@gmail.com>
2022-01-14 21:58:49 +0100

    Fixed Checkstyle issues

--- src/main/java/h10/MyLinkedList.java
+++ src/main/java/h10/MyLinkedList.java
@@ -1,6 +1,11 @@
 package h10;
 
+/**
+ * An object of this class represents a Linked List.
+ *
+ * @param <T> Type ListItem Objects saved in the LinkedList
+ */
 public class MyLinkedList<T> {
     public ListItem<T> head;
-    
+
 }
