3e093c378646dba7a7e4cec199b12703f56a52da
Tobias Durst <33527599+xuri02@users.noreply.github.com>
2022-01-21 15:30:45 +0100

    Added "add" method to MyLinkedList (#2)

--- src/main/java/h10/MyLinkedList.java
+++ src/main/java/h10/MyLinkedList.java
@@ -8,4 +8,25 @@ package h10;
 public class MyLinkedList<T> {
     public ListItem<T> head;
 
+    /**
+     * This method adds a new list item with parameter "key" as its key value to the end of the list.
+     *
+     * @param key key value of item to be added
+     * @return boolean stating if the item has been added to the list successfully
+     */
+    public boolean add(T key) {
+        if (head == null) {
+            head = new ListItem<>(key);
+            return true;
+        }
+
+        ListItem<T> p = head;
+
+        while (p.next != null) {
+            p = p.next;
+        }
+
+        p.next = new ListItem<>(key);
+        return true;
+    }
 }
